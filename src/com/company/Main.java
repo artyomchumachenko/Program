37b20c2357579c736330d929dd887e455e284672
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-03-15 12:25:27 +0300

    add "hello my friends"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello Lena!");
+        System.out.println("Hello Vanya!");
+        System.out.println("Hello Vanya Two!");
     }
 }
