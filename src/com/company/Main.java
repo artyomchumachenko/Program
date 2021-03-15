178222494dd14981e01ba2ca444c9ecbee00ef25
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-03-15 14:17:19 +0300

    add "Hello World!"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Lena!");
-        System.out.println("Hello Vanya!");
-        System.out.println("Hello Vanya Two!");
+    public static void main(String... args) {
+        System.out.println("Hello World!");
     }
 }
