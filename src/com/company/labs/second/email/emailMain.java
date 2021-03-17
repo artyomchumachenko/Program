7fcd8ccdfec3ea10a69f1ae6b5952df8c5ac9ac0
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-03-17 23:50:49 +0300

    regex for IPv6 upd 17.03.21 *upd with limited length <64 <255

--- src/com/company/labs/second/email/emailMain.java
+++ src/com/company/labs/second/email/emailMain.java
@@ -8,16 +8,16 @@ public class emailMain {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         String emailString = new String();
-        String regexEmail = new String("^[a-zA-Z0-9]+[\\_\\-\\+\\.]*[a-zA-Z0-9]+\\@((([a-zA-Z]+\\.)+[a-zA-Z]+)"
-                + "|(((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]|[0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]"
-                + "|[0-9]?)))$");
+        String regexEmail = new String("^([a-zA-Z0-9][\\_\\-\\+\\.]{0,1}[a-zA-Z0-9]){1,32}\\@"
+                + "((([a-zA-Z]+\\.)+[a-zA-Z]+)|(((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]|[0-9]?)\\.){3}"
+                + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]|[0-9]?)))$");
         System.out.println("Введите email: ");
         emailString = scanner.nextLine();
         scanner.close();
-        Pattern patternEmail = Pattern.compile(regexEmail);
-        Matcher matcherEmail = patternEmail.matcher(emailString);
-        if (matcherEmail.find()) {
-            System.out.println("true");
+        if (emailString.length() < 256) {
+            Pattern patternEmail = Pattern.compile(regexEmail);
+            Matcher matcherEmail = patternEmail.matcher(emailString);
+            System.out.println(matcherEmail.find());
         } else {
             System.out.println("false");
         }
