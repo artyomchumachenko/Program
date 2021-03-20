994992e4a6f7ea8c6340046ce26cf4db06fcf8ff
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-03-20 09:10:31 +0300

    upd URL and Email regex thx Vanya 20.03.21

--- src/com/company/labs/second/email/EmailMain.java
+++ src/com/company/labs/second/email/EmailMain.java
@@ -8,9 +8,10 @@ public class EmailMain {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         String emailString = new String();
-        String regexEmail = new String("[a-zA-Z0-9]+[\\_\\-\\+\\.]*[a-zA-Z0-9]+\\@((([a-zA-Z]+\\.)+[a-zA-Z]+)"
-                + "|(((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]|[0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]"
-                + "|[0-9]?)))");
+        String regexEmail = new String("^(?=(.{1,64}@))(?=(.{7,255}$))((([a-zA-Z0-9_а-яА-ЯёЁ]+)"
+                + "(\\+[a-zA-Z0-9_а-яА-ЯёЁ\\-_]+)?)@(([a-zA-Z0-9а-яА-ЯёЁ][a-zA-Z0-9_а-яА-ЯёЁ\\-_]*\\.)+"
+                + "([a-zA-Zа-яА-ЯёЁ0-9]{2,})|(((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}"
+                + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]))))$");
         System.out.println("Введите email: ");
         emailString = scanner.nextLine();
         scanner.close();
--- src/com/company/labs/second/url/UrlMain.java
+++ src/com/company/labs/second/url/UrlMain.java
@@ -8,10 +8,11 @@ public class UrlMain {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         String addressString;
-        String regexAddressString = new String("^((http:|https:)//)?[A-Za-z0-9][-A-Za-z0-9]*"
-                + "[A-Za-z0-9](\\\\.[A-Za-z0-9][-A-Za-z0-9]*[A-Za-z0-9])*(:[0-9]+)?"
-                + "(/([A-Za-z0-9._]|%20)+)*(\\\\?([A-Za-z0-9]|%20)+=([A-Za-z0-9]|%20)+"
-                + "(&([A-Za-z0-9]|%20)+=([A-Za-z0-9]|%20)+)*)?(#[A-Za-z0-9]+)?$");
+        String regexAddressString = new String("^(https://|http://)?([a-zA-Z0-9а-яА-ЯёЁ][a-zA-Z0-9_а-яА-ЯёЁ\\-_]*"
+                + "\\.)+([a-zA-Zа-яА-ЯёЁ0-9]{2,})(:(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|"
+                + "[1-9][0-9]{3}|[1-9][0-9]{2}|[1-9][0-9]|[1-9]))?(/[a-zA-Z0-9а-яА-ЯёЁ][a-zA-Z0-9_\\-а-яА-ЯёЁ]*)*"
+                + "(/?\\?[a-zA-Z0-9\\-а-яА-ЯёЁ]+=[a-zA-Z0-9\\-а-яА-ЯёЁ]+(&[a-zA-Z0-9\\-а-яА-ЯёЁ]+=[a-zA-Z0-9\\-а-яА-ЯёЁ]+)*)*"
+                + "(/?#[a-zA-Zа-яА-ЯёЁ0-9_\\-]+)*/?$");
         System.out.println("Введите URL: ");
         addressString = scanner.nextLine();
         scanner.close();
