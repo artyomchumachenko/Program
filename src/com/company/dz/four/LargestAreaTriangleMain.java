40a845c801d172460ddda204b2ee8feed24ca556
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-03-25 09:43:19 +0300

    maxArea вроде работает

--- src/com/company/dz/four/LargestAreaTriangleMain.java
+++ src/com/company/dz/four/LargestAreaTriangleMain.java
@@ -9,8 +9,11 @@ public class LargestAreaTriangleMain {
         int numberOfPoints = 0;
         int[] x;
         int[] y;
-        boolean flag = false;
-        int[] lengthSide;
+        boolean flag;
+        double maxArea = 0;
+        double currArea = 0;
+        double halfPerimeter = 0;
+        double[] length = new double[3];
 
         System.out.println("Введите количество точек: ");
         do {
@@ -39,5 +42,33 @@ public class LargestAreaTriangleMain {
             scanner.nextLine();
         }
 
+        for (int i = 0; i < numberOfPoints; i++) {
+            if (i == numberOfPoints - 2) {
+                length[0] = Math.sqrt(Math.pow(x[i + 1] - x[i], 2) + Math.pow(y[i + 1] - y[i], 2));
+                length[1] = Math.sqrt(Math.pow(x[0] - x[i + 1], 2) + Math.pow(y[0] - y[i + 1], 2));
+                length[2] = Math.sqrt(Math.pow(x[i] - x[0], 2) + Math.pow(y[i] - y[0], 2));
+            } else if (i == numberOfPoints - 1) {
+                length[0] = Math.sqrt(Math.pow(x[0] - x[i], 2) + Math.pow(y[0] - y[i], 2));
+                length[1] = Math.sqrt(Math.pow(x[1] - x[0], 2) + Math.pow(y[1] - y[0], 2));
+                length[2] = Math.sqrt(Math.pow(x[i] - x[1], 2) + Math.pow(y[i] - y[1], 2));
+            } else {
+                length[0] = Math.sqrt(Math.pow(x[i + 1] - x[i], 2) + Math.pow(y[i + 1] - y[i], 2));
+                length[1] = Math.sqrt(Math.pow(x[i + 2] - x[i + 1], 2) + Math.pow(y[i + 2] - y[i + 1], 2));
+                length[2] = Math.sqrt(Math.pow(x[i] - x[i + 2], 2) + Math.pow(y[i] - y[i + 2], 2));
+            }
+            halfPerimeter = (length[0] + length[1] + length[2]) / 2;
+            currArea = Math.sqrt(halfPerimeter *
+                    (halfPerimeter - length[0]) *
+                    (halfPerimeter - length[1]) *
+                    (halfPerimeter - length[2]));
+            if (currArea > maxArea) {
+                maxArea = currArea;
+            }
+            length[0] = 0;
+            length[1] = 0;
+            length[2] = 0;
+            currArea = 0;
+        }
+        System.out.printf("%.2f", maxArea);
     }
 }
