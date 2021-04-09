02ed440b350247e1664611f1086d228cbe0f8678
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-04-09 10:33:01 +0300

    READY

--- src/com/company/dz/six/forwardingmessages/ForwardingMessagesMain.java
+++ src/com/company/dz/six/forwardingmessages/ForwardingMessagesMain.java
@@ -2,30 +2,41 @@ package com.company.dz.six.forwardingmessages;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ForwardingMessagesMain {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> emails = new ArrayList<>();
-        ArrayList<String> mainChain = new ArrayList<>();
-        final String end = new String("^END$");
+        HashMap<String, String> pairOfMailboxes = new HashMap<>();
         boolean flag = true;
 
         do {
-            System.out.println("Введите строку");
-            String inputString = scanner.nextLine();
-            if (inputString.matches(end)) {
-                flag = false;
-            } else {
-                emails.add(inputString.replace(":", " > "));
+            System.out.println("Введите пару почт");
+            String inputPairOfMailboxes = scanner.nextLine();
+            if (!inputPairOfMailboxes.matches("END")) {
+                String[] splitPairMails = inputPairOfMailboxes.split(":");
+                pairOfMailboxes.put(splitPairMails[0], splitPairMails[1]);
                 flag = true;
+            } else {
+                flag = false;
             }
         } while (flag);
-        for (String email : emails) {
-            System.out.println(email);
-            System.out.println("--------------");
+        ArrayList<String> firstSender = new ArrayList<>();
+        for (String currMail : pairOfMailboxes.keySet()) {
+            if (!pairOfMailboxes.containsValue(currMail)) {
+                firstSender.add(currMail);
+            }
+        }
+        for (String currMail : firstSender) {
+            StringBuilder currResult = new StringBuilder();
+            currResult.append(currMail);
+            do {
+                currMail = pairOfMailboxes.get(currMail);
+                currResult.append(" > ").append(currMail);
+            } while (pairOfMailboxes.containsKey(currMail));
+            System.out.println(currResult.toString());
         }
     }
 }
